fb4240a7b2e1776ba6fdc1c235269f5a19c33297
Gregory Senyshyn <gregory.senyshyn@gmail.com>
2016-12-27 01:04:26 -0500

    section grading now works. Next is to make whole test grade itself

--- src/js/osslt.js
+++ src/js/osslt.js
@@ -1,18 +1,22 @@
-var score = null;
-
-function resetScore() {
+function getScore() {
   score = {'direct': {'correct':0, 'incorrect':0},
              'indirect': {'correct':0, 'incorrect':0}};
+  return score
 }
 
-function mark(section){
+function mark(section, score){
 
   var sectionElement = document.getElementById('section' + section);
-  var sectionLength = sectionElement.getElementsByTagName('li').length;
-  for (var i=0; i<sectionLength; i++){
+  var questionCount = 0;
+  for (node in sectionElement.childNodes) {
+    if (sectionElement.childNodes[node].nodeName === "LI") {
+      questionCount++;
+    }
+  }
+  for (var i=0; i<questionCount; i++){
     var question = document.getElementById('question-' + section + '-' + i)
+    var inputs = document.getElementsByName('question-' + section + '-' + i);
     var checkedAnswer = false;
-    var inputs = question.getElementsByTagName('input');
     for (x=0; x<inputs.length; x++) {
       if (inputs[x].checked === true) {
         checkedAnswer = true;
@@ -36,72 +40,84 @@ function mark(section){
   }
 }
 
-  // var directScore = Math.round(score.direct.correct /
-  //                     (score.direct.correct + score.direct.incorrect) * 100);
-  // if (isNaN(directScore)){
-  //   directScore = 'N/A';
-  // }
-  // else {
-  //   directScore = directScore + '%';
-  // }
+function calculateScores (score) {
+  var directScore = Math.round(score.direct.correct /
+                      (score.direct.correct + score.direct.incorrect) * 100);
+  if (isNaN(directScore)){
+    directScore = 'N/A';
+  }
+  else {
+    directScore = directScore + '%';
+  }
 
-  // var indirectScore = Math.round(score.indirect.correct /
-  //                       (score.indirect.correct +
-  //                        score.indirect.incorrect)*100);
-  // if (isNaN(indirectScore)){
-  //   indirectScore = 'N/A';
-  // }
-  // else {
-  //   indirectScore = indirectScore + '%';
-  // }
+  var indirectScore = Math.round(score.indirect.correct /
+                        (score.indirect.correct +
+                         score.indirect.incorrect)*100);
+  if (isNaN(indirectScore)){
+    indirectScore = 'N/A';
+  }
+  else {
+    indirectScore = indirectScore + '%';
+  }
 
-  // var totalScore = Math.round((score.direct.correct +
-  //                              score.indirect.correct) /
-  //                              (score.direct.correct +
-  //                               score.direct.incorrect +
-  //                               score.indirect.correct +
-  //                               score.indirect.incorrect) * 100);
-  // if (isNaN(totalScore)){
-  //   totalScore = 'N/A';
-  // }
-  // else {
-  //   totalScore = totalScore + '%';
-  // }
+  var totalScore = Math.round((score.direct.correct +
+                               score.indirect.correct) /
+                               (score.direct.correct +
+                                score.direct.incorrect +
+                                score.indirect.correct +
+                                score.indirect.incorrect) * 100);
+  if (isNaN(totalScore)){
+    totalScore = 'N/A';
+  }
+  else {
+    totalScore = totalScore + '%';
+  }
 
-  // document.getElementById('direct').innerHTML = (
-  //   'Direct: ' +
-  //   score.direct.correct +
-  //   '/' +
-  //   (score.direct.correct +
-  //   score.direct.incorrect) +
-  //   ' (' + directScore + ')'
-  //   );
-  // document.getElementById('indirect').innerHTML = (
-  //   'Indirect: ' +
-  //   score.indirect.correct +
-  //   '/' +
-  //   (score.indirect.correct +
-  //   score.indirect.incorrect) +
-  //   ' (' + indirectScore + ')'
-  //   );
-  // document.getElementById('total').innerHTML = (
-  //   'Total: ' +
-  //   (score.indirect.correct +
-  //   score.direct.correct) +
-  //   '/' +
-  //   (score.direct.correct +
-  //   score.direct.incorrect +
-  //   score.indirect.correct +
-  //   score.indirect.incorrect) +
-  //   ' (' + totalScore + ')'
-  //   );
+  document.getElementById('direct').innerHTML = (
+    'Direct: ' +
+    score.direct.correct +
+    '/' +
+    (score.direct.correct +
+    score.direct.incorrect) +
+    ' (' + directScore + ')'
+    );
+  document.getElementById('indirect').innerHTML = (
+    'Indirect: ' +
+    score.indirect.correct +
+    '/' +
+    (score.indirect.correct +
+    score.indirect.incorrect) +
+    ' (' + indirectScore + ')'
+    );
+  document.getElementById('total').innerHTML = (
+    'Total: ' +
+    (score.indirect.correct +
+    score.direct.correct) +
+    '/' +
+    (score.direct.correct +
+    score.direct.incorrect +
+    score.indirect.correct +
+    score.indirect.incorrect) +
+    ' (' + totalScore + ')'
+    );
+}
 
-  // $('#score').foundation('reveal', 'open');
-  // $(document).foundation();
+function showScores() {
+  var modal = document.getElementById('score-modal')
+  modal.style.display = 'flex';
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+}
+
 
 function markSection(section) {
-  resetScore();
-  mark(section);
+  var score = getScore();
+  mark(section, score);
+  calculateScores(score);
+  showScores();
 }
 
 
