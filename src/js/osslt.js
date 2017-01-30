82caf239cab1735ea59d2e182dfa81c801373e9f
Gregory Senyshyn <gregory.senyshyn@gmail.com>
2017-01-29 21:52:29 -0500

    adding progress bar to written answers in osslt

--- src/js/osslt.js
+++ src/js/osslt.js
@@ -4,6 +4,93 @@ function getScore() {
   return score
 }
 
+
+function updateCount(section, question, questionType) {
+  var benchmarks = {"short":[35, 50, 60], 
+                    "news report": [35, 50, 65, 100, 175, 250], 
+                    "opinion": [45, 80, 115, 200, 300, 450]};
+  var response = document.getElementById("response-" + section + "-" + question);
+  var arrow = document.getElementById("wc-arrow-" + section + "-" + question);
+  var matches = response.value.match(/[\w\d]+/gi);
+  var count = matches ? matches.length : 0;
+  var wordCount = document.getElementById("wc-" + section + "-" + question);
+  wordCount.innerHTML = count;
+  var arrowMargin = null;
+  if (questionType == 1){
+    if (count <= benchmarks.short[0]) {
+      arrowMargin = 33 * (count/benchmarks.short[0]);
+    }
+    else if (benchmarks.short[0] < count && count <= benchmarks.short[1]) {
+      arrowMargin = (33 * ((count - benchmarks.short[0])/(benchmarks.short[1]-benchmarks.short[0]))) + 33;
+    }
+    else if (benchmarks.short[1] < count && count <= benchmarks.short[2]) {
+      arrowMargin = (33 * ((count - benchmarks.short[1])/(benchmarks.short[2]-benchmarks.short[1]))) + 66;
+    }
+    else if (benchmarks.short[2] < count) {
+      arrowMargin = 100; 
+    }
+  }
+  else if (questionType == 2 || questionType == 3){
+      
+    var milestones = null;
+    if (questionType == 2) {
+      milestones = benchmarks["news report"];
+    }
+    else if (questionType == 3) {
+      milestones = benchmarks.opinion;
+    }
+
+    if (count <= milestones[0]) {
+      arrowMargin = 16.5 * (count/milestones[0]);
+    }
+    else if (milestones[0] < count && count <= milestones[1]) {
+      arrowMargin = (16.5 * ((count - milestones[0])/(milestones[1]-milestones[0]))) + 16.5;
+    }
+    else if (milestones[1] < count && count <= milestones[2]) {
+      arrowMargin = (16.5 * ((count - milestones[1])/(milestones[2]-milestones[1]))) + 33;
+    }
+    else if (milestones[2] < count && count <= milestones[3]) {
+      arrowMargin = (16.5 * ((count - milestones[2])/(milestones[3]-milestones[2]))) + 50;
+    }
+    else if (milestones[3] < count && count <= milestones[4]) {
+      arrowMargin = (16.5 * ((count - milestones[3])/(milestones[4]-milestones[3]))) + 66.5;
+    }
+    else if (milestones[4] < count && count <= milestones[5]) {
+      arrowMargin = (16.5 * ((count - milestones[4])/(milestones[5]-milestones[4]))) + 83.5;
+    }
+    else if (milestones[5] < count) {
+      arrowMargin = 100; 
+    }
+  }
+  arrow.style.marginLeft = arrowMargin + "%"; 
+}
+
+
+function markMC(question, inputs, score) {
+  var checkedAnswer = false;
+  for (x=0; x<inputs.length; x++) {
+    if (inputs[x].checked === true) {
+      checkedAnswer = true;
+      if (inputs[x].value === question.dataset.answer){
+        score[question.dataset.subtype].correct++;
+        question.classList.remove("incorrect");
+        question.classList.add("correct");
+      }
+      else {
+        score[question.dataset.subtype].incorrect++;
+        question.classList.remove("correct");
+        question.classList.add("incorrect");
+      }
+    }
+  }
+  if (checkedAnswer === false) {
+    score[question.dataset.subtype].incorrect++;
+    question.classList.remove("correct");
+    question.classList.add("incorrect");
+  }
+}
+
+
 function mark(section, score){
 
   var sectionElement = document.getElementById('section' + section);
@@ -15,28 +102,13 @@ function mark(section, score){
   }
   for (var i=0; i<questionCount; i++){
     var question = document.getElementById('question-' + section + '-' + i)
-    var inputs = document.getElementsByName('question-' + section + '-' + i);
-    var checkedAnswer = false;
-    for (x=0; x<inputs.length; x++) {
-      if (inputs[x].checked === true) {
-        checkedAnswer = true;
-        if (inputs[x].value === question.dataset.answer){
-          score[question.dataset.type].correct++;
-          question.classList.remove("incorrect");
-          question.classList.add("correct");
-        }
-        else {
-          score[question.dataset.type].incorrect++;
-          question.classList.remove("correct");
-          question.classList.add("incorrect");
-        }
-      }
+
+    var mcQuestionTypes = ["normal", "identification", "combination"];
+    if (mcQuestionTypes.indexOf(question.dataset.type) > -1) {
+      var inputs = document.getElementsByName('question-' + section + '-' + i);
+      markMC(question, inputs, score);
     }
-    if (checkedAnswer === false) {
-      score[question.dataset.type].incorrect++;
-      question.classList.remove("correct");
-      question.classList.add("incorrect");
-    }
+
   }
 }
 
@@ -121,8 +193,11 @@ function markSection(section) {
 }
 
 
-function markTest() {
-  resetScore();
-  window.alert("fix this, retard");
-  // mark(section);
+function markTest(sectionCount) {
+  var score = getScore();
+  for (var i=0; i<sectionCount; i++) {
+    mark(i, score);
+  }
+  calculateScores(score);
+  showScores();
 }
