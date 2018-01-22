a9f87f57edc1cbca4ac9f84dff43ada20a3cd5ac
Gregory Senyshyn <gregory.senyshyn@gmail.com>
2018-01-22 10:46:11 -0500

    making syncImages delete files not in source dir

--- syncImages.sh
+++ syncImages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avh "/Volumes/GoogleDrive/Team Drives/Yearbook 2017-2018/Prod Files/images/" images
+rsync -avh "/Volumes/GoogleDrive/Team Drives/Yearbook 2017-2018/Prod Files/images/" images --delete --exclude "*.gdoc"
 chmod -R 755 images
